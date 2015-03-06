17551f054c41922b83beb4b9517c286d1938eb55
Seres <sunnylu@dyn-209-2-215-230.dyn.columbia.edu>
2015-03-06 17:17:31 -0500

    updated popup information

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
-var map = L.map('map').setView([40.714269,-74.005973], 12);
+var map = L.map('leafletMap').setView([40.714269, -74.005973], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var mappingDataArray = mappingData.rows;
+var centers = mappingData.rows;
 
-for (var i=0; i<mappingDataArray.length; i++) {
-	var locationObj = mappingDataArray[i];
-	var marker = L.marker(locationObj.latitude,locationObj.longitude]).addTo(map);
+for (var i = 0; i < centers.length; i++) {
+	var locationObj = centers[i];
 
-var marker = L.marker([40.712261,-74.00492]).addTo(map);
+	var marker = L.marker(locationObj.latitude, locationObj.longitude).addTo(map);
 
-	marker.bindPopup(+locationObj.NameofCenter+"<br>"+locationObj.FullAddress);
+	marker.bindPopup("<b>" + locationObj.NameofCenter + "</b><br>" + locationObj.FullAddress);
+} // end of for loop
